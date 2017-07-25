639a64c5ce214388df1c2e02d21de4e6ffec5f0e
alun <alunfeixue@qq.com>
2017-07-25 15:01:57 +0800

    commit

--- library/src/main/java/com/daohen/social/library/qq/QQProvider.java
+++ library/src/main/java/com/daohen/social/library/qq/QQProvider.java
@@ -3,6 +3,7 @@ package com.daohen.social.library.qq;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.daohen.personal.toolbox.library.Singleton;
 import com.daohen.personal.toolbox.library.util.Contexts;
@@ -12,7 +13,10 @@ import com.daohen.social.library.qq.listener.LoginListener;
 import com.daohen.social.library.qq.listener.UserInfoIUiListener;
 import com.tencent.connect.UserInfo;
 import com.tencent.connect.common.Constants;
+import com.tencent.connect.share.QQShare;
+import com.tencent.tauth.IUiListener;
 import com.tencent.tauth.Tencent;
+import com.tencent.tauth.UiError;
 
 /**
  * CREATE BY ALUN
@@ -24,14 +28,16 @@ public class QQProvider {
     private Tencent tencent;
     private Context context;
     private String appid;
+    private String appname;
 
     public static final QQProvider get(){
         return gDefault.get();
     }
 
-    public void registerQQ(Context context, String appid){
+    public void registerQQ(Context context, String appid, String appname){
         this.context = context;
         this.appid = appid;
+        this.appname = appname;
         tencent = Tencent.createInstance(appid, context);
     }
 
@@ -63,9 +69,57 @@ public class QQProvider {
         userInfo.getUserInfo(new UserInfoIUiListener(listener));
     }
 
-    public void shareQQ(){
+    public void shareLocalImage(Activity activity, String path, IUiListener listener){
         Bundle bundle = new Bundle();
+        bundle.putInt(QQShare.SHARE_TO_QQ_KEY_TYPE, QQShare.SHARE_TO_QQ_TYPE_IMAGE);
+        bundle.putString(QQShare.SHARE_TO_QQ_IMAGE_LOCAL_URL, path);
+        bundle.putString(QQShare.SHARE_TO_QQ_APP_NAME, appname);
+        share(activity, bundle, listener);
+    }
 
+    public void shareDefault(Activity activity, String title, String summary, String targetUrl, String imageUrl, IUiListener listener){
+        Bundle bundle = new Bundle();
+        bundle.putInt(QQShare.SHARE_TO_QQ_KEY_TYPE, QQShare.SHARE_TO_QQ_TYPE_DEFAULT);
+        bundle.putString(QQShare.SHARE_TO_QQ_TITLE, title);
+        bundle.putString(QQShare.SHARE_TO_QQ_SUMMARY, summary);
+        bundle.putString(QQShare.SHARE_TO_QQ_TARGET_URL, targetUrl);
+        bundle.putString(QQShare.SHARE_TO_QQ_IMAGE_URL, imageUrl);
+        bundle.putString(QQShare.SHARE_TO_QQ_APP_NAME, appname);
+        share(activity, bundle, listener);
+    }
+
+    public void shareApp(Activity activity, String title, String summary, String targetUrl, String imageUrl, IUiListener listener){
+        Bundle bundle = new Bundle();
+        bundle.putInt(QQShare.SHARE_TO_QQ_KEY_TYPE, QQShare.SHARE_TO_QQ_TYPE_APP);
+        bundle.putString(QQShare.SHARE_TO_QQ_TITLE, title);
+        bundle.putString(QQShare.SHARE_TO_QQ_SUMMARY, summary);
+        bundle.putString(QQShare.SHARE_TO_QQ_TARGET_URL, targetUrl);
+        bundle.putString(QQShare.SHARE_TO_QQ_IMAGE_URL, imageUrl);
+        bundle.putString(QQShare.SHARE_TO_QQ_APP_NAME, appname);
+        share(activity, bundle, listener);
+    }
+
+    public void shareAudio(Activity activity, String title, String summary, String targetUrl, String imageUrl, String audioUrl, IUiListener listener){
+        Bundle bundle = new Bundle();
+        bundle.putInt(QQShare.SHARE_TO_QQ_KEY_TYPE, QQShare.SHARE_TO_QQ_TYPE_AUDIO);
+        bundle.putString(QQShare.SHARE_TO_QQ_TITLE, title);
+        bundle.putString(QQShare.SHARE_TO_QQ_SUMMARY, summary);
+        bundle.putString(QQShare.SHARE_TO_QQ_TARGET_URL, targetUrl);
+        bundle.putString(QQShare.SHARE_TO_QQ_IMAGE_URL, imageUrl);
+        bundle.putString(QQShare.SHARE_TO_QQ_APP_NAME, appname);
+        bundle.putString(QQShare.SHARE_TO_QQ_AUDIO_URL, audioUrl);
+        share(activity, bundle, listener);
+    }
+
+    private void share(final Activity activity, final Bundle bundle, final IUiListener listener){
+        checkNull();
+
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                tencent.shareToQQ(activity, bundle, listener);
+            }
+        });
     }
 
     private static final Singleton<QQProvider> gDefault = new Singleton<QQProvider>() {
